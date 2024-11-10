237d59c420c27ec9d300cf33ce8d5fd13974ed0d
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-11-10 20:59:07 +0530

    04-built in modules

--- index.js
+++
@@ -1,9 +0,0 @@
-// const sum=require('./add.js')
-// const details=require('./details.js')
-// const name=require('./person.js')
-import greet from './greet.mjs'
-
-//console.log(details)  //{ a: 'Aswinee', b: 23, c: 'male' }
-console.log(greet)
-// sum(2,3)
-// console.log("welcome to index.js")
